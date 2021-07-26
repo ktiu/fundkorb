dbff62153a5462792465ad0091390e134627fa20
ktiu <git@ktiu.net>
2021-07-26 13:36:33 +0200

    remove search img

--- assets/js/initializeSearch.js
+++ assets/js/initializeSearch.js
@@ -6,7 +6,7 @@ var sjs = SimpleJekyllSearch({
   searchInput:          document.getElementById('sucheingabe'),
   resultsContainer:     document.getElementById('suchergebnisse'),
   json:                 '{{ "assets/json/searchIndex.json" | relative_url }}',
-  searchResultTemplate: '<a href="{url}"><li><img src="{profilbild}"/><div><span class="wissenschaftlich">{wissenschaftlich}</span> <span class="deutsch">({deutsch})</span></div></li></a>',
+  searchResultTemplate: '<a href="{url}"><li><div><span class="wissenschaftlich">{wissenschaftlich}</span> <span class="deutsch">({deutsch})</span></div></li></a>',
   fuzzy:                false,
   noResultsText:        '<li class="p-2 text-muted">Keine Pilze gefunden.<li>'
 })
