67ca2bbc39357d2e3ac0ddfee49a906332a5c623
ktiu <git@ktiu.net>
2021-09-28 17:26:34 +0200

    shuffle for real

--- assets/js/shuffleQuiz.js
+++ assets/js/shuffleQuiz.js
@@ -19,5 +19,16 @@
 
 $(function() {
   $("#quiz .carousel-inner > div").shuffle();
-  $("#quiz .carousel-inner > div").first().addClass("active");
+  var first = $("#quiz .carousel-inner > div").first();
+  first.addClass("active");
+  var firstImg = first.find("img[data-src]");
+  firstImg.attr('src', firstImg.data('src'));
+  firstImg.removeAttr('data-src');
+
+  $('.carousel.lazy-load').bind('slide.bs.carousel', function (e) {
+    var image = $(e.relatedTarget).find('img[data-src]');
+    image.attr('src', image.data('src'));
+    image.removeAttr('data-src');
+  });
+
 });
